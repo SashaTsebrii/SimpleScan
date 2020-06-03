6ece1ab9cd4dc3c72716d8829554cc00d7b1d043
SashaTsebrii <sashatsebrii@gmail.com>
2020-06-03 19:57:39 +0300

    Initial commit.

---
+++ SimpleScan/ViewController.swift
@@ -0,0 +1,209 @@
+//
+//  ViewController.swift
+//  SimpleScan
+//
+//  Created by Aleksandr Tsebrii on 6/3/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+import VisionKit
+import PDFKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    // MARK: Variables
+    
+    var wasScaned: Bool = false
+    
+    // MARK: Properties
+    
+    var scanButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .lightGray
+        button.setTitle("Scan", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(scanButtoTapped(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var pdfButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .lightGray
+        button.setTitle("PDF", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.gray, for: .disabled)
+        button.addTarget(self, action: #selector(pdfButtonTapped(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    var pdfView: PDFView = {
+        let pdfView = PDFView()
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        return pdfView
+    }()
+    
+    // MARK: Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        
+        view.addSubview(pdfView)
+        NSLayoutConstraint.activate([
+            pdfView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            pdfView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            pdfView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            pdfView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        let stackView = UIStackView(arrangedSubviews: [scanButton, pdfButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 8
+        stackView.backgroundColor = .red
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.heightAnchor.constraint(equalToConstant: 64),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        ])
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: Actions
+    
+    @objc func scanButtoTapped(_ sender: UIButton) {
+        
+        #if targetEnvironment(simulator)
+        // Simulator
+        let alert = UIAlertController(title: "Camera not available", message: "You are using a simulator, the camera is available only on a real device.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { _ in
+            
+        })
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+        #else
+        // Real device
+        let scannerViewController = VNDocumentCameraViewController()
+        scannerViewController.delegate = self
+        present(scannerViewController, animated: true)
+        #endif
+        
+    }
+    
+    @objc func pdfButtonTapped(_ sender: UIButton) {
+        
+        if wasScaned {
+            
+            let fileManager = FileManager.default
+            if let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first {
+                
+                let docURL = documentDirectory.appendingPathComponent(Constants.fileName)
+                if fileManager.fileExists(atPath: docURL.path) {
+                    pdfView.document = PDFDocument(url: docURL)
+                } else {
+                    print("Error load file from URL")
+                }
+                
+            }
+            
+        } else {
+            
+            let pdfDocument = PDFDocument()
+            if let tifImage = UIImage(named: "simple.tif") {
+                if let pdfPage = PDFPage(image: tifImage) {
+                    pdfDocument.insert(pdfPage, at: 0)
+                    pdfView.document = pdfDocument
+                }
+            }
+            
+        }
+        
+    }
+    
+}
+
+extension ViewController: VNDocumentCameraViewControllerDelegate {
+    
+    // MARK: VNDocumentCameraViewControllerDelegate
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        guard scan.pageCount >= 1 else {
+            controller.dismiss(animated: true)
+            return
+        }
+        
+        DispatchQueue.main.async {
+            
+            let pdfDocument = PDFDocument()
+            
+            for i in 0 ..< scan.pageCount {
+                // Set image size
+                if let image = scan.imageOfPage(at: i).resize(toWidth: 800) {
+                    print("Image size is \(image.size.width), \(image.size.height)")
+                    // Create a PDF page instance from your image
+                    let pdfPage = PDFPage(image: image)
+                    // Insert the PDF page into your document
+                    pdfDocument.insert(pdfPage!, at: i)
+                }
+            }
+            
+            // Get the raw data of your PDF document
+            let data = pdfDocument.dataRepresentation()
+            
+            let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let documentURL = documentDirectory.appendingPathComponent(Constants.fileName)
+            do {
+                try data?.write(to: documentURL)
+            } catch (let error) {
+                print("Error save data to URL: \(error.localizedDescription)")
+            }
+        }
+        
+        controller.dismiss(animated: true)
+        
+        wasScaned = true
+        
+    }
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+        print("Error in CameraViewController: \(error)")
+        controller.dismiss(animated: true)
+    }
+    
+    func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+        controller.dismiss(animated: true)
+    }
+    
+}
+
+// MARK: Constants
+
+struct Constants {
+    static let fileName = "Last_scanned_document.pdf"
+}
+
+// MARK: UIImage extension
+
+extension UIImage {
+    
+    func resize(toWidth width: CGFloat) -> UIImage? {
+        let canvas = CGSize(width: width, height: CGFloat(ceil(width / size.width * size.height)))
+        return UIGraphicsImageRenderer(size: canvas, format: imageRendererFormat).image {
+            _ in draw(in: CGRect(origin: .zero, size: canvas))
+        }
+    }
+    
+}
