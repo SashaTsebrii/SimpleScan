bc0ed992caaf1bc2c63d2edf9426d22b13102206
SashaTsebrii <sashatsebrii@gmail.com>
2020-06-04 22:33:04 +0300

    Implement PreviewController.

---
+++ SimpleScan/PreviewController.swift
@@ -0,0 +1,21 @@
+//
+//  PreviewController.swift
+//  SimpleScan
+//
+//  Created by Aleksandr Tsebrii on 6/4/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+class PreviewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set title
+        title = NSLocalizedString("Preview", comment: "")
+
+    }
+    
+}
