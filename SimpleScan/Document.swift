ed463ce5881769604fa93fd9f029cf1f631c053c
SashaTsebrii <sashatsebrii@gmail.com>
2020-06-04 22:32:47 +0300

    Implement Document.

---
+++ SimpleScan/Document.swift
@@ -0,0 +1,15 @@
+//
+//  Document.swift
+//  SimpleScan
+//
+//  Created by Aleksandr Tsebrii on 6/4/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import Foundation
+
+struct Document {
+    var name: String?
+    var createDate: Date?
+    var pdfUrl: URL?
+}
