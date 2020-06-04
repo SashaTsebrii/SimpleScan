0e463ec326f55f912e5653973878002fbdeda7a4
SashaTsebrii <sashatsebrii@gmail.com>
2020-06-04 22:33:15 +0300

    Implement SettingsController.

---
+++ SimpleScan/SettingsController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsController.swift
+//  SimpleScan
+//
+//  Created by Aleksandr Tsebrii on 6/4/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       // Set title
+       title = NSLocalizedString("Settings", comment: "")
+        
+    }
+    
+}
