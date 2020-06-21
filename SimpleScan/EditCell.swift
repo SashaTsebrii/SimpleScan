94a2fe854357f033e75f42298fb1c0f3faf62682
SashaTsebrii <sashatsebrii@gmail.com>
2020-06-21 14:50:33 +0300

    Added page separator in EditCell. Added moveImageView and deleteButton in EditCell.

--- SimpleScan/EditCell.swift
+++ SimpleScan/EditCell.swift
@@ -29,13 +29,17 @@ class EditCell: UICollectionViewCell {
         didSet {
             if let pageOfPages = pageOfPages {
                 pageNumberLabel.text = "\(String(pageOfPages.current + 1))/\(String(pageOfPages.total))"
+                
+                if pageOfPages.current + 1 != pageOfPages.total {
+                    drawDashLine(start: CGPoint(x: contentView.bounds.minX, y: contentView.bounds.maxX), end: CGPoint(x: contentView.bounds.maxX, y: contentView.bounds.maxY), view: contentView)
+                }
             }
         }
     }
     
     // MARK: Properties
     
-    var pdfView: PDFView = {
+    fileprivate let pdfView: PDFView = {
         let pdfView = PDFView()
         pdfView.backgroundColor = .clear
         pdfView.autoScales = true
@@ -47,13 +51,28 @@ class EditCell: UICollectionViewCell {
         return pdfView
     }()
     
-    fileprivate var pageNumberLabel: UILabel = {
+    fileprivate let pageNumberLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16)
         label.textColor = .gray
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    fileprivate let moveImageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.image = UIImage(named: "move")
+        imageView.isUserInteractionEnabled = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    fileprivate let deleteButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setImage(UIImage(named: "delete"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     // MARK: Initialization
@@ -67,7 +86,7 @@ class EditCell: UICollectionViewCell {
         NSLayoutConstraint.activate([
             pageNumberLabel.heightAnchor.constraint(equalToConstant: 18),
             pageNumberLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            pageNumberLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            pageNumberLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
         ])
         
         contentView.addSubview(pdfView)
@@ -75,7 +94,23 @@ class EditCell: UICollectionViewCell {
             pdfView.topAnchor.constraint(equalTo: contentView.topAnchor),
             pdfView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             pdfView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            pdfView.bottomAnchor.constraint(equalTo: pageNumberLabel.topAnchor)
+            pdfView.bottomAnchor.constraint(equalTo: pageNumberLabel.topAnchor, constant: -4)
+        ])
+        
+        contentView.addSubview(moveImageView)
+        NSLayoutConstraint.activate([
+            moveImageView.widthAnchor.constraint(equalToConstant: 32),
+            moveImageView.heightAnchor.constraint(equalTo: moveImageView.widthAnchor),
+            moveImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+            moveImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0)
+        ])
+        
+        contentView.addSubview(deleteButton)
+        NSLayoutConstraint.activate([
+            deleteButton.widthAnchor.constraint(equalToConstant: 32),
+            deleteButton.heightAnchor.constraint(equalTo: deleteButton.widthAnchor),
+            deleteButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+            deleteButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0)
         ])
         
     }
@@ -83,5 +118,20 @@ class EditCell: UICollectionViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-        
+    
+    // MARK: Helper
+    
+    func drawDashLine(start p0: CGPoint, end p1: CGPoint, view: UIView) {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.strokeColor = UIColor.lightGray.cgColor
+        shapeLayer.lineWidth = 1
+        // 5 is the length of dash & 5 is length of the gap
+        shapeLayer.lineDashPattern = [5, 5]
+
+        let path = CGMutablePath()
+        path.addLines(between: [p0, p1])
+        shapeLayer.path = path
+        view.layer.addSublayer(shapeLayer)
+    }
+    
 }
