b8961198df050bffd2a3fba5f795fea649d1f7a2
SashaTsebrii <sashatsebrii@gmail.com>
2020-06-23 03:20:20 +0300

    Implement draw logic in AnnotationController.

--- SimpleScan/AnnotationController.swift
+++ SimpleScan/AnnotationController.swift
@@ -15,8 +15,6 @@ class AnnotationController: UIViewController {
     
     var document: Document?
     
-    fileprivate let canvas = Canvas()
-    
     // MARK: Prpperties
     
     fileprivate let pdfView: PDFView = {
@@ -28,6 +26,69 @@ class AnnotationController: UIViewController {
         pdfView.pageBreakMargins = UIEdgeInsets(top: 16, left: 16, bottom: 0, right: 16)
         pdfView.translatesAutoresizingMaskIntoConstraints = false
         return pdfView
+    }()
+    
+    fileprivate let canvasView: CanvasView = {
+        let canvasView = CanvasView()
+        canvasView.backgroundColor = .clear
+        canvasView.translatesAutoresizingMaskIntoConstraints = false
+        return canvasView
+    }()
+    
+    fileprivate let undoButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 32, height: 32))
+        button.setImage(UIImage(named: "undo"), for: .normal)
+        button.addTarget(self, action: #selector(undoButtonTapped(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let clearButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 32, height: 32))
+        button.setImage(UIImage(named: "clear"), for: .normal)
+        button.addTarget(self, action: #selector(clearButtonTapped(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    let redButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 32, height: 32))
+        button.layer.cornerRadius = 16
+        button.layer.masksToBounds = true
+        button.backgroundColor = .red
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: #selector(handleColorChange(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let greenButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 32, height: 32))
+        button.layer.cornerRadius = 16
+        button.layer.masksToBounds = true
+        button.backgroundColor = .green
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: #selector(handleColorChange(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let blueButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 32, height: 32))
+        button.layer.cornerRadius = 16
+        button.layer.masksToBounds = true
+        button.backgroundColor = .blue
+        button.layer.borderWidth = 1
+        button.addTarget(self, action: #selector(handleColorChange(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let slider: UISlider = {
+        let slider = UISlider()
+        slider.minimumValue = 1
+        slider.maximumValue = 10
+        slider.addTarget(self, action: #selector(handleSliderChange(_:)), for: .valueChanged)
+        return slider
     }()
     
     // MARK: Lifecycle
@@ -48,6 +109,35 @@ class AnnotationController: UIViewController {
             pdfView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             pdfView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             pdfView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        // Canvas view constraints
+        view.addSubview(canvasView)
+        NSLayoutConstraint.activate([
+            canvasView.topAnchor.constraint(equalTo: pdfView.topAnchor),
+            canvasView.leadingAnchor.constraint(equalTo: pdfView.leadingAnchor),
+            canvasView.trailingAnchor.constraint(equalTo: pdfView.trailingAnchor),
+            canvasView.bottomAnchor.constraint(equalTo: pdfView.bottomAnchor)
+        ])
+        
+        let colorsStack = UIStackView(arrangedSubviews: [redButton, greenButton, blueButton])
+        colorsStack.spacing = 4
+        colorsStack.distribution = .fillEqually
+        
+        let buttonsStack = UIStackView(arrangedSubviews: [undoButton, clearButton])
+        buttonsStack.spacing = 4
+        buttonsStack.distribution = .fillEqually
+        
+        let generalStack = UIStackView(arrangedSubviews: [buttonsStack, colorsStack, slider])
+        generalStack.spacing = 8
+        generalStack.distribution = .fillEqually
+        generalStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(generalStack)
+        NSLayoutConstraint.activate([
+            generalStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            generalStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            generalStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
         
         // Create save bar button item
@@ -92,7 +182,7 @@ class AnnotationController: UIViewController {
         } else {
             print("Error no document")
         }
-        
+        /*
         guard let page = pdfView.currentPage else {return}
         // Create a rectangular path
         // Note that in PDF page coordinate space, (0,0) is the bottom left corner of the page
@@ -100,15 +190,29 @@ class AnnotationController: UIViewController {
         let inkAnnotation = PDFAnnotation(bounds: page.bounds(for: pdfView.displayBox), forType: .ink, withProperties: nil)
         inkAnnotation.add(path)
         page.addAnnotation(inkAnnotation)
-        
+        */
     }
     
     // MARK: Actions
     
     @objc fileprivate func saveBarButtonTapped(_ sender: UIButton) {
-        
         navigationController?.popViewController(animated: true)
-        
+    }
+    
+    @objc fileprivate func undoButtonTapped(_ sender: UIButton) {
+        canvasView.undo()
+    }
+    
+    @objc func clearButtonTapped(_ sender: UIButton) {
+        canvasView.clear()
+    }
+    
+    @objc fileprivate func handleColorChange(_ sender: UIButton) {
+        canvasView.setStrokeColor(color: sender.backgroundColor ?? .black)
+    }
+    
+    @objc fileprivate func handleSliderChange(_ sender: UISlider) {
+        canvasView.setStrokeWidth(width: sender.value)
     }
     
 }
---
+++ SimpleScan/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  SimpleScan
+//
+//  Created by Aleksandr Tsebrii on 6/23/20.
+//  Copyright © 2020 Aleksandr Tsebrii. All rights reserved.
+//
+
+import UIKit
+
+struct Line {
+    let strokeWidth: Float
+    let color: UIColor
+    var points: [CGPoint]
+}
